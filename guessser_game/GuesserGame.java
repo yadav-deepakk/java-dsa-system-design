70d25c0ce058457d235518772d51ba5d977ad93e
yadav-deepakk <deepakyadav53882@gmail.com>
2023-02-05 04:48:31 +0530

    GuesserGame: handled multiple winners in umpire class

--- guessser_game/GuesserGame.java
+++ guessser_game/GuesserGame.java
@@ -7,9 +7,9 @@ public class GuesserGame{
 
     private Scanner sc = new Scanner(System.in);
     private String userChoice = null;
-    int playerCount = -1;
-    ArrayList<Integer> guessList = new ArrayList<Integer>();
-    ArrayList<String> playerNames = new ArrayList<String>();
+    private int playerCount = -1;
+    private ArrayList<Integer> guessList = new ArrayList<Integer>();
+    private ArrayList<String> playerNames = new ArrayList<String>();
 
     GuesserGame(){}
 
@@ -21,13 +21,16 @@ public class GuesserGame{
         System.out.println("****** WELCOME TO GUESSER GAME ******");
 
         while(true){
-            System.out.print("Do you want to continue: (Yes or No) -> ");
+            System.out.print("Will you play GuesserGame: (Yes or No) -> ");
             this.userChoice = sc.next();
             if(this.userChoice != null && (this.userChoice.equalsIgnoreCase("yes") || this.userChoice.equalsIgnoreCase("y")))
                 this.playGame();
-            else {
+            else if(this.userChoice != null && (this.userChoice.equalsIgnoreCase("no") || this.userChoice.equalsIgnoreCase("n")))
+            {
                 this.exitGame();
                 break;
+            }else{
+                System.out.println("Please enter a correct choice!!!");
             }
         }
 
@@ -38,13 +41,13 @@ public class GuesserGame{
         this.playerCount = this.sc.nextInt();
         for(int i=0; i<this.playerCount;i++){
             System.out.println("Player " + (i+1) + " enter your name and number.");
-            playerNames.add(sc.next());
-            guessList.add(sc.nextInt());
+            this.playerNames.add(sc.next());
+            this.guessList.add(sc.nextInt());
         }
         GuessGenerator guessGenerator = new GuessGenerator();
         Umpire ump = new Umpire();
-        ump.announceWinner(playerNames, guessList, guessGenerator.returnGeneratedNum(2));
-        // ump.announceWinner(playerNames, guessList, 34);
+        ump.announceWinner(this.playerNames, this.guessList, guessGenerator.returnGeneratedNum(2));
+        // ump.announceWinner(this.playerNames, this.guessList, 34);
 
     }
 
@@ -58,8 +61,7 @@ public class GuesserGame{
         try{
             game.launchGame();
         }catch(Exception e){
-            System.out.println(e.toString());
-            System.out.print("Above Exception Occured...");
+            System.out.print("Exception " + e.toString() + "occured during program execution.");
         }
     }
 
--- guessser_game/Umpire.java
+++ guessser_game/Umpire.java
@@ -5,18 +5,21 @@ import java.util.ArrayList;
 public class Umpire {
 
     public void announceWinner(ArrayList<String> playerNames, ArrayList<Integer> palyerGuesses, int numToBeGuessed){
-        int flag = 0; 
+        ArrayList<String> winnerList = new ArrayList<String>();
+
         for(int i=0; i<playerNames.size(); i++){
             if(palyerGuesses.get(i) == numToBeGuessed) {
-                flag = 1;
-                System.out.println(playerNames.get(i) + " has guessed correct number."); 
+                winnerList.add(playerNames.get(i));
             } 
         }
-        System.out.println("The number was: " + numToBeGuessed);
-        if(flag == 0){
+        System.out.println("The number to be guessed was: " + numToBeGuessed);
+        if(winnerList.size() == 0){
             System.out.println("No one guessed correct number. \nBetter Luck next time.");
+        }else{
+            System.out.print("Winner(s): ");
+            winnerList.forEach(winner -> System.out.print(winner + " "));
+            System.out.println();
         }
-      
     }
     
 }
